f1326cb0358bf44fb22cec773cc25b6fecbfa3af
Stefan Edwards <saedwards.ecc@gmail.com>
2016-12-24 09:37:20 -0500

    pep8/flake8 love for srv.py

--- srv.py
+++ srv.py
@@ -1,11 +1,11 @@
-from bottle import app, route, request, response, run
-import cgi
+from bottle import route, request, run
 import time
 
 queue = []
 who = []
 tmpl = None
 MAX = 10
+
 
 @route('/')
 def index():
@@ -14,6 +14,7 @@ def index():
         with file('index.html', 'r') as fh:
             tmpl = fh.read()
     return tmpl
+
 
 @route('/message', method=['get', 'post'])
 def message():
@@ -34,25 +35,27 @@ def message():
     else:
         return {"queue": queue}
 
+
 @route('/who', method=['post'])
 def updateWho():
-	global who
-	data = request.json
-	user = data['user']
-	ts = time.time()
-	ip = request.remote_addr
-	who.append(dict(user=user, ts=ts, ip=ip))	
-	# Check timestamp from last update from user and purge.
-	for u in who:
-		lapse = time.time() - u['ts'] 
-		# print lapse, u
-		if lapse > 50:
-			who.remove(u)
-		else:
-			for uu in who:
-				if u != uu and u['user'] == uu['user'] and u['ip'] == uu['ip']:
-					who.remove(uu)
-	# print who
-	return {"who": who}
+    global who
+    data = request.json
+    user = data['user']
+    ts = time.time()
+    ip = request.remote_addr
+    who.append(dict(user=user, ts=ts, ip=ip))
+    # Check timestamp from last update from user and purge.
+    for u in who:
+        lapse = time.time() - u['ts']
+        # print lapse, u
+        if lapse > 50:
+            who.remove(u)
+        else:
+            for uu in who:
+                if u != uu and u['user'] == uu['user'] and u['ip'] == uu['ip']:
+                    who.remove(uu)
+    # print who
+    return {"who": who}
+
 
 run(host='0.0.0.0', port=8085)
